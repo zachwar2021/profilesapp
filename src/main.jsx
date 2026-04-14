409ee4bf7f56a272051268af403e573442bd329a
Zachary Warren <zacharywarren2021@gmail.com>
2026-04-13 23:57:37 -0500

    adding name to page

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from "./App.jsx";
 import './index.css'
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, Heading, View } from "@aws-amplify/ui-react";
+
+const components = {
+  Header() {
+    return (
+      <View textAlign="center" padding="2rem 0 1rem 0">
+        <Heading level={1}>Zachary Warren</Heading>
+      </View>
+    );
+  },
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Authenticator>
+    <Authenticator components={components}>
       <App />
     </Authenticator>
   </StrictMode>
